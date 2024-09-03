cfff8b06355d395ba3acaf71b038193b05de2f97
Irnam Soimansoib <irnam.sa@gmail.com>
2024-09-03 13:13:58 +0200

    Style the public layout

--- src/components/PublicLayout.tsx
+++ src/components/PublicLayout.tsx
@@ -1,11 +1,37 @@
-import { Heading } from "@chakra-ui/react";
+import { Grid, GridItem, Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import PublicHeader from "./PublicHeader";
 
 const PublicLayout = () => {
   return (
     <>
-      <Heading>PublicLayout</Heading>
-      <Outlet />
+      <Grid
+        templateAreas={{
+          base: `"header"
+               "main"`,
+        }}
+        templateRows={"90px 1fr"}
+        templateColumns="1fr"
+        minHeight="100vh"
+        minWidth="100vw"
+      >
+        <GridItem bg="white" area={"header"}>
+          <PublicHeader />
+        </GridItem>
+
+        <GridItem bg="white" area={"main"}>
+          <Box
+            w="100%"
+            h="100%"
+            paddingTop="50px"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+          >
+            <Outlet />
+          </Box>
+        </GridItem>
+      </Grid>
     </>
   );
 };
