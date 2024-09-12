25d2487a6f579aeca2e9ab4110adc32049bacd23
Irnam Soimansoib <irnam.sa@gmail.com>
2024-09-12 18:02:59 +0200

    Build the dashboard layout

--- src/components/DashLayout.tsx
+++ src/components/DashLayout.tsx
@@ -1,16 +1,41 @@
-import { Heading, Box, Spacer } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import DashNavBar from "./DashNavBar";
+import DashHeader from "./DashHeader";
 import { Outlet } from "react-router-dom";
-import { Link } from "react-router-dom";
+
 const DashLayout = () => {
   return (
     <>
-      <Heading>DashLayout</Heading>
-      <Box>
-        <Link to="outings">See all events</Link>
-        <Spacer />
-        <Link to="user">See your profile</Link>
-      </Box>
-      <Outlet />
+      <Grid
+        templateAreas={{
+          base: `"header" "main" "footer"`,
+          sm: `"header header"
+                  "nav main"
+                  "nav footer"`,
+          lg: `"header header"
+                  "nav main"
+                  "nav footer"`,
+        }}
+        templateRows={"60px 1fr 30px"}
+        templateColumns={{ base: "1fr", sm: "150px 1fr" }}
+        minHeight="100vh"
+      >
+        <GridItem bg="white" area={"header"}>
+          <DashHeader />
+        </GridItem>
+
+        <Show above="sm">
+          <GridItem bg="white" area={"nav"}>
+            <DashNavBar />
+          </GridItem>
+        </Show>
+        <GridItem bg="white" area={"main"} padding={3}>
+          <Outlet />
+        </GridItem>
+        <GridItem bg="white" area={"footer"}>
+          Footer
+        </GridItem>
+      </Grid>
     </>
   );
 };
